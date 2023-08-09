799a761774a3bdd51e26489ca4da83a1d558fa95
Jonathan1295-09 <jcasasola955@gmail.com>
2023-08-09 17:21:47 -0500

    indexCreateRoutes

--- server.js
+++ server.js
@@ -44,6 +44,26 @@ app.use(express.json());
 
 // ROUTES
 
+//Index//
+app.get("/bookmarks", async(req,res) => {
+    try{
+        const bookmark = await Bookmarks.find({})
+        res.json(bookmark)
+    }catch (error){
+        res.status
+    }
+});
+
+//Create//
+app.post("/bookmarks", async (req,res) => {
+    try{
+        const bookmark = await bookmark.create(req.body)
+        res.json(bookmark)
+    }catch (error){
+        res.status(400).json({error})
+    }
+});
+
 // UPDATE - PUT 
 app.put("/bookmarks/:id", async (req, res) => {
     try {
